29352997c7c45aa1c21c4c9beaf0d3252db61e4a
shubhamk29 <shubhamk2907@gmail.com>
2024-05-20 22:41:10 +0530

    Refactor - 1

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <footer className="py-10 text-center px-6">
             <div>
-                <p className="text-sm font-normal">Built by <a href="https://github.com/ShubhamK-2907" className="underline">Shubhzz</a>. source code avialable on <a className="underline" href="https://github.com/ShubhamK-2907/shadcn-cookie-consent">github</a>.</p>
+                <p className="text-sm font-normal">Built by <a href="https://github.com/ShubhamK-2907" className="underline">Shubhzz</a>. source code available on <a className="underline" href="https://github.com/ShubhamK-2907/shadcn-cookie-consent">github</a>.</p>
             </div>
         </footer>
     )
